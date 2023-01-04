bba4a2f77d1d015f216c98e5cb74b24b2768ca8d
DaviBT <davi.cesarbt@gmail.com>
2023-01-04 15:50:46 -0300

    adding javascript to the form

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,21 @@
-function sent(){
-    let txtnum = window.document.getElementById('inum')
-    let num = Number(txtnum)
+let num = document.querySelector('input#inum')
+let list = document.querySelector('select#ilist')
+let res = document.querySelector('div#res')
+let numbers = [Number(num.value)]
 
-    
+// verifying if the number is already in the list
+function inList(n, l){ 
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
+
+function add(){
+    if(inList(num.value, numbers)){
+        window.alert('Sorry, the element is incorrect or it is already in the list')
+    }else{
+        window.alert('ok')
+    }
 }
